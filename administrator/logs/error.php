0c17ab001b395c78d27ff23af4f63bacfb8db5d4
Ganusha93 <mkgjmadusanka@gmail.com>
2016-11-20 21:05:06 +0530

    commit 1

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-20 12:24:50 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-20T12:24:50+00:00	INFO ::1	joomlafailure	Empty password not allowed.
